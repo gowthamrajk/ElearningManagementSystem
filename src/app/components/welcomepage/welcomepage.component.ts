f0c3dc8ddc561a5be6dad17caffde57767f88ae0
GowthamRaj K <43011442+gowthamrajk@users.noreply.github.com>
2021-08-23 13:53:38 +0530

    Update welcomepage.component.ts

--- src/app/components/welcomepage/welcomepage.component.ts
+++ src/app/components/welcomepage/welcomepage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-welcomepage',
@@ -12,7 +13,42 @@ export class WelcomepageComponent implements OnInit {
 
   ngOnInit(): void 
   {
-    
+    $("#subtext1").click(function(){
+      $("#innertext1").slideToggle(300);
+      if($('#innertext2').is(':visible')||$('#innertext3').is(':visible')||$('#innertext4').is(':visible'))
+      {
+        $('#innertext2').slideUp(300);
+        $('#innertext3').slideUp(300);
+        $('#innertext4').slideUp(300);
+      }
+  });
+  $("#subtext2").click(function(){
+      $("#innertext2").slideToggle(300);
+      if($('#innertext1').is(':visible')||$('#innertext3').is(':visible')||$('#innertext4').is(':visible'))
+      {
+        $('#innertext1').slideUp(300);
+        $('#innertext3').slideUp(300);
+        $('#innertext4').slideUp(300);
+      }
+  });
+  $("#subtext3").click(function(){
+      $("#innertext3").slideToggle(300);
+      if($('#innertext1').is(':visible')||$('#innertext2').is(':visible')||$('#innertext4').is(':visible'))
+      {
+        $('#innertext1').slideUp(300);
+        $('#innertext2').slideUp(300);
+        $('#innertext4').slideUp(300);
+      }
+  });
+  $("#subtext4").click(function(){
+      $("#innertext4").slideToggle(300);
+      if($('#innertext1').is(':visible')||$('#innertext2').is(':visible')||$('#innertext3').is(':visible'))
+      {
+        $('#innertext1').slideUp(300);
+        $('#innertext2').slideUp(300);
+        $('#innertext3').slideUp(300);
+      }
+  }); 
   }
 
   navigate()
